351c2f52248ba6cd5d027746f1dbe0da8ee78b97
James <jamesyoun710@gmail.com>
2013-09-27 12:59:39 -0700

    hard code static_pages. no erb

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -5,21 +5,36 @@ describe "StaticPages" do
   describe "Home Page" do
     it "should have the content 'Sample App'" do
     	visit '/static_pages/home'
-    	page.should have_content('Sample App')
+    	page.should have_selector('h1', :text => 'Sample App')
     end
+	it "should have the right title" do
+	  visit '/static_pages/home'
+	  page.should have_selector('title',
+	                    :text => "Ruby on Rails Tutorial Sample App | Home")
+	end    
   end
 
   describe "Help Page" do
   	it "should have the content 'Help'" do
   		visit '/static_pages/help'
-  		page.should have_content('Help')
+  		page.should have_selector('h1', :text => 'Help')
   	end
+  	it "should have the right title" do
+	  visit '/static_pages/help'
+	  page.should have_selector('title',
+		                  :text => " | Help")# substrings are OK.  This will pass without specifying the whole title
+	end
   end
 
   describe "About Page" do
   	it "should have the content 'about'" do
-  		visit '/static_pages/about'
-  		page.should have_content('About')
+  	  visit '/static_pages/about'
+  	  page.should have_selector('h1', :text => 'About')
   	end
+	it "should have the right title" do
+	  visit '/static_pages/about'
+	  page.should have_selector('title',
+	                    :text => "Ruby on Rails Tutorial Sample App | About")
+	end
   end
 end
