41a6111ff0f7958ddbfd3553b4b284c65ebe981c
James <jamesyoun710@gmail.com>
2013-09-30 17:20:50 -0700

    add link tests

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -3,6 +3,22 @@ require 'spec_helper'
 describe "Static pages" do
   subject { page }
 
+  it "should have the right links on the layout" do
+    visit root_path
+    click_link "About"
+    page.should have_selector 'title', text: full_title('About')
+    click_link "Help"
+    page.should have_selector 'title', text: full_title('Help')
+    click_link "Contact"
+    page.should have_selector 'title', text: full_title('Contact')
+    click_link "Home"
+    page.should_not have_selector 'title', text: full_title('Home')
+    click_link "Sign up now!"
+    page.should have_selector 'title', text: full_title('Sign up')
+    click_link "sample app"
+    page.should_not have_selector 'title', text: full_title('Home')
+  end
+
   describe "Home page" do
     before { visit root_path }
 
