47009a2c4c0373777630ca5c9bd183f8d73ede55
James <jamesyoun710@gmail.com>
2013-10-09 10:17:22 -0700

    stage for chapter 11.  add wrapper helper

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -20,7 +20,7 @@ class MicropostsController < ApplicationController
 
     private
       def correct_user
-      @micropost = current_user.microposts.find_by_id(params[:id])
-      redirect_to root_url if @micropost.nil?
-    end
+        @micropost = current_user.microposts.find_by_id(params[:id])
+        redirect_to root_url if @micropost.nil?
+      end
 end
